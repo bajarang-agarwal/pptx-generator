c03241746bcf1be3135b8f3e2b41ff9bbe692783
bajarang-agarwal <freelance.techno@gmail.com>
2021-07-15 17:12:05 +0530

    issue fix

---
+++ ppt-index.js
@@ -0,0 +1,7 @@
+require("@babel/polyfill");
+
+const package = {};
+package.Presentation = require('./build/Presentation').default;
+package.Slide = require('./build/Slide').default;
+
+module.exports = package;
